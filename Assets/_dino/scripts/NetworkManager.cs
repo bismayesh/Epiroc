581e96d5a04516dece2f1feba75109730993c75d
d3noDL <dino@adenogame.com>
2023-05-10 17:19:15 +0200

    NetworkManager
    - Created a network manager
    - Connects to server
    - Joins a lobby
    - Creates or joins a room depending on if a room already exits
    - Set up external plugs
    - Spawn player on random location

--- Assets/_dino/scripts/NetworkManager.cs
+++ Assets/_dino/scripts/NetworkManager.cs
@@ -1,18 +1,63 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
-public class NetworkManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class NetworkManager : MonoBehaviourPunCallbacks {
+
+    public GameObject playerPrefab;
+    public List<Transform> playerSpawnLocations;
+    
+    private void Start() {
+        Debug.Log("Connecting to master server...");
+        PhotonNetwork.ConnectUsingSettings();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public override void OnConnectedToMaster() {
+        Debug.Log("<color=green><b>Connected!</b></color>");
+        Debug.Log("Joining lobby...");
+        PhotonNetwork.JoinLobby();
     }
+
+    public override void OnJoinedLobby() {
+        Debug.Log("<color=green><b>Joined lobby!</b></color>");
+        if (PhotonNetwork.CountOfRooms == 0) {
+            Debug.Log("Couldn't find an active room... Creating a new one...");
+            PhotonNetwork.CreateRoom("dev");
+        }
+        else {
+            Debug.Log("Joining room...");
+            PhotonNetwork.JoinRoom("dev");
+        }
+    }
+
+    public override void OnCreatedRoom() {
+        Debug.Log("<color=green><b>Successfully created a room!</b></color>");
+    }
+    
+    public override void OnJoinedRoom() {
+        if (PhotonNetwork.IsMasterClient) {
+            SpawnPlayers();
+        }
+        else {
+            Debug.Log("<color=green><b>Successfully joined a room!</b></color>");
+            SpawnPlayers();
+        }
+    }
+
+    public void SpawnPlayers() {
+        var playerSpawnLocationIndex = (int)Random.Range(0, playerSpawnLocations.Count - 1);
+
+        Debug.Log($"Spawning on <color=yellow>{playerSpawnLocations[playerSpawnLocationIndex].position}</color>");
+        PhotonNetwork.Instantiate(
+            playerPrefab.name, 
+            playerSpawnLocations[playerSpawnLocationIndex].position, 
+            Quaternion.identity
+            );
+    }
+    
 }
+
