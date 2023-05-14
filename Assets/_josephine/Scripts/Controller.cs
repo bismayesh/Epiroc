448c28476e9d2f05ebaf06d06f0c9a185b4a79a7
7quino <josephine.johnzon@hyperisland.se>
2023-05-14 08:55:41 +0200

    Alteration controller script

--- Assets/_josephine/Scripts/Controller.cs
+++ Assets/_josephine/Scripts/Controller.cs
@@ -7,6 +7,8 @@ public class Controller : MonoBehaviour
     public Training activeTraining;
     public Task activeTask;
 
+    bool pressing = false;
+
     private void Start()
     {
         activeTraining = GameObject.FindGameObjectWithTag("Training").GetComponent<Training>();
@@ -15,10 +17,19 @@ public class Controller : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player" && !pressing)
         {
+            pressing = true;
             Debug.Log("Player pressed button");
+
+            StartCoroutine(WaitTwoSeconds());
         }
+    }
+
+    private IEnumerator WaitTwoSeconds()
+    {
+        yield return new WaitForSeconds(2);
+        pressing = false;
     }
 
     public void ButtonFunction()
