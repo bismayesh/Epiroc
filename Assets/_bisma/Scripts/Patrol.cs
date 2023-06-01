7fa2cfddca71f6a633895cd299977cbec29be764
7quino <josephine.johnzon@hyperisland.se>
2023-06-01 13:58:20 +0200

    Working on troll ai

--- Assets/_bisma/Scripts/Patrol.cs
+++ Assets/_bisma/Scripts/Patrol.cs
@@ -3,62 +3,78 @@
     using System.Collections;
 
 
-    public class Patrol : MonoBehaviour {
-    [HideInInspector]
-        public Transform Gem=null;
-        public Transform[] points;
-        private int destPoint = 0;
-        private NavMeshAgent agent;
-       [SerializeField] private bool Gemspawned = false; 
+public class Patrol : MonoBehaviour
+{
+//[HideInInspector]
+    public Transform Gem = null;
+    public Transform[] points;
+    private int destPoint = 0;
+    private NavMeshAgent agent;
+    [SerializeField]
+    private bool Gemspawned = false; 
 
 
-        void Start () {
-            agent = GetComponent<NavMeshAgent>();
+    void Start ()
+    {
+        agent = GetComponent<NavMeshAgent>();
 
-            // Disabling auto-braking allows for continuous movement
-            // between points (ie, the agent doesn't slow down as it
-            // approaches a destination point).
-            agent.autoBraking = false;
+        // Disabling auto-braking allows for continuous movement
+        // between points (ie, the agent doesn't slow down as it
+        // approaches a destination point).
+        agent.autoBraking = false;
 
-            GotoNextPoint();
-        }
+        GotoNextPoint();
+    }
 
 
-        void GotoNextPoint() {
-            // Returns if no points have been set up
-            if (points.Length == 0)
-                return;
-        agent.destination = points[destPoint].position;
+    void GotoNextPoint()
+    {
+        Debug.Log("Next waypoint");
+
+        // Returns if no points have been set up
+        if (points.Length == 0)
+            return;
+        
 
         // Set the agent to go to the currently selected destination.
         if (!Gemspawned)
         {
-            //agent.destination = points[destPoint].position;
+            agent.destination = points[destPoint].position;
         }
             else
         {
-            Gem.position = GameObject.FindGameObjectWithTag("ChunkGem").transform.position;
-            //agent.destination = Gem.position;
+            Gem = GameObject.FindGameObjectWithTag("ChunkGem").transform;
+            agent.destination = Gem.position;
         }
 
-            // Choose the next point in the array as the destination,
-            // cycling to the start if necessary.
-            destPoint = (destPoint + 1) % points.Length;
-        }
+        // Choose the next point in the array as the destination,
+        // cycling to the start if necessary.
+        destPoint = (destPoint + 1) % points.Length;
+    }
 
+    void Update ()
+    {
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+            GotoNextPoint();
 
-        void Update () {
-           
-            if (!agent.pathPending && agent.remainingDistance < 0.5f)
-                GotoNextPoint();
-
-        if (GameObject.FindGameObjectsWithTag("ChunkGem") != null)
+        if (GameObject.FindGameObjectWithTag("ChunkGem") != null)
         {
-            Gemspawned = true;
-        }
-        else Gemspawned = false;
-        Debug.Log(agent.remainingDistance);
+            //Activate if statemant when cave colliders are in place
+            //Gemspawned = true;
 
+        }
+        else
+        {
+            Gemspawned = false;
+            Gem = null;
+        }
     }
-            
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "ChunkGem")
+        {
+            Destroy(collision.gameObject);
+        }
     }
+}
