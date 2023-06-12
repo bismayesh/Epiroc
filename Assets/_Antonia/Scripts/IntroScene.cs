541a4233cce0d020cf539cfb352f4c3a58804116
Antonia Cecilia <126273091+antoniacecilia@users.noreply.github.com>
2023-06-12 14:07:22 +0200

    Intro Scene/Scene Transition/UI/Sound
    Added: Script for scene transition, placeholder buttons for language settings, Epiroc and HI logos, Sound effect files

--- Assets/_Antonia/Scripts/IntroScene.cs
+++ Assets/_Antonia/Scripts/IntroScene.cs
@@ -3,9 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.XR.Interaction.Toolkit;
-
+using UnityEngine.InputSystem;
 public class IntroScene : MonoBehaviour
 {
+    public InputActionProperty primaryButtonAction;
+    float primaryButtonValue;
+    public void FixedUpdate()
+    {
+        primaryButtonValue = primaryButtonAction.action.ReadValue<float>();
+        if (primaryButtonValue != 0.0f)
+        {
+            TransitionToMainScene();
+        }
+    }
     public void TransitionToMainScene()
     {
         SceneManager.LoadScene(1);
